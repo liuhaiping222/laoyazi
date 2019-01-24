cd4892e72aae800f12072bb45f9d764d18d9368c
sunruiming <1988srmS@s>
2019-01-24 10:32:40 +0800

    第二次提交

--- test01/src/main/java/com/dohow/demo.java
+++ test01/src/main/java/com/dohow/demo.java
@@ -3,6 +3,7 @@ package com.dohow;
 public class demo {
     public static void main() {
         System.out.println("yayaya");
+        System.out.println("123");
         System.out.println("yayaya123");
     }
 }
